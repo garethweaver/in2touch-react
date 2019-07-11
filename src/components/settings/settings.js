e6e8379dd8f30431877fcb71b1e31b4c58f39743
garethweaver <reg@garethweaver.com>
2019-07-11 12:23:43 +0100

    feature(settings and help): add info sections

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -7,10 +7,35 @@ const getDate = str => new Date(str).toString()
 
 const Settings = ({ ...props }) => (
   <Layout>
-    <h2>Database updated at:</h2>
-    <div>{getDate(props.config.updatedAt)}</div>
-
-    <div className="ButtonRack">
+    <h1 className="Margin--b">Settings</h1>
+    <section className="Section">
+      <h2>Latest database update</h2>
+      <p>{getDate(props.config.updatedAt)}</p>
+    </section>
+    <section className="Section">
+      <h2>Add to homescreen</h2>
+      <p>To add this app to your homescreen:</p>
+      <p><strong>On IOS</strong> click the share button and find the 'add to homescreen' grey icon.</p>
+      <p><strong>On Android</strong> click the chrome menu button and then 'add to homescreen'.</p>
+    </section>
+    <section className="Section">
+      <h2>Free to use!</h2>
+      <p>
+        This is a free to use app created
+        by <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.garethweaver.com">
+          Gareth Weaver
+        </a>. You can message me on <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/garethdweaver">
+          twitter
+        </a> if you have any questions or comments.
+      </p>
+    </section>
+    <section className="Section">
       <button
         onClick={props.handleClick}
         className="Btn__flex">
@@ -19,7 +44,7 @@ const Settings = ({ ...props }) => (
           sync
         </span>
       </button>
-    </div>
+    </section>
   </Layout>
 )
 
