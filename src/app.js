1a0a458e69d4ef263fe4f9f58e8b772388a72abb
garethweaver <reg@garethweaver.com>
2019-07-23 11:13:22 +0100

    feature(offline indicator)

--- src/app.js
+++ src/app.js
@@ -5,12 +5,30 @@ import Header from './components/header/header'
 import firebaseListener from './store/actions/firebase-listener'
 import ScrollToTop from './components/layouts/scroll-top'
 import Container from './components/container/container'
+import Offline from './components/notifiers/offline'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { isOnline: true }
+  }
 
   componentWillMount() {
     this.props.dispatch(firebaseListener())
   }
+
+  componentDidMount() {
+    window.addEventListener('online', () => this.setOnlineStatus(true))
+    window.addEventListener('offline', () => this.setOnlineStatus(false))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online')
+    window.removeEventListener('offline')
+  }
+
+  setOnlineStatus = isOnline => this.setState({ isOnline })
 
   render() {
     return (
@@ -20,6 +38,9 @@ class App extends Component {
           <Container />
           <div className="Pagebg" />
         </ScrollToTop>
+        {!this.state.isOnline &&
+          <Offline />
+        }
       </Router>
     )
   }
---
+++ src/components/notifiers/offline.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './offline.sass'
+
+const Notifiers = () => (
+  <div className="Offline">
+    You're offline, usage is limited
+  </div>
+)
+
+export default Notifiers
