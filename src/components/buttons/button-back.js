3f7089a7bd5cd481ee6d09faca7e5a9e31a4bd52
garethweaver <reg@garethweaver.com>
2019-07-29 10:34:25 +0100

    bugfix(back button for ios)

--- src/components/buttons/button-back.js
+++ src/components/buttons/button-back.js
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router'
 const ButtonBack = ({ ...props }) => (
   <a
     href="#back"
-    onClick={() => props.history.goBack()}
+    onClick={e => { e.preventDefault(); props.history.goBack() }}
     className="Btn__home">
     <span className="material-icons">
       arrow_back
