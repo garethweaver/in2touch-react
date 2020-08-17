419879d3cc8004eed51cc45be62953f5e7dcad3c
garethweaver <reg@garethweaver.com>
2020-08-17 09:34:35 +0100

    Updates

---
+++ precache-manifest.d0d6b7b0ad304c2b6b3785cc0ac78d63.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c133f1dd8a22791690853f20c0ea475e",
+    "url": "/in2touch-react/index.html"
+  },
+  {
+    "revision": "fcb1da9802ae2e611d74",
+    "url": "/in2touch-react/static/css/2.88f8224b.chunk.css"
+  },
+  {
+    "revision": "f1da57f5bc4e245ce722",
+    "url": "/in2touch-react/static/css/main.b852da18.chunk.css"
+  },
+  {
+    "revision": "fcb1da9802ae2e611d74",
+    "url": "/in2touch-react/static/js/2.3e41537c.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/in2touch-react/static/js/2.3e41537c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1da57f5bc4e245ce722",
+    "url": "/in2touch-react/static/js/main.ed3bab8e.chunk.js"
+  },
+  {
+    "revision": "783e667b99a290f3e20f",
+    "url": "/in2touch-react/static/js/runtime-main.9c3449c3.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/in2touch-react/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/in2touch-react/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/in2touch-react/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/in2touch-react/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "1fe9cc2170c678e8c1f8c5e3535ab848",
+    "url": "/in2touch-react/static/media/inconsolata-latin-400.1fe9cc21.woff"
+  },
+  {
+    "revision": "3b892623781be87c5d9c0baaf120282f",
+    "url": "/in2touch-react/static/media/inconsolata-latin-400.3b892623.woff2"
+  },
+  {
+    "revision": "8caa728e7fabd0a8927567953a46e3c2",
+    "url": "/in2touch-react/static/media/inconsolata-latin-700.8caa728e.woff2"
+  },
+  {
+    "revision": "ca5a6a8c1d3e9185ff972bf537768122",
+    "url": "/in2touch-react/static/media/inconsolata-latin-700.ca5a6a8c.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/in2touch-react/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/in2touch-react/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/in2touch-react/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/in2touch-react/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/in2touch-react/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/in2touch-react/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/in2touch-react/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/in2touch-react/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/in2touch-react/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/in2touch-react/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/in2touch-react/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/in2touch-react/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/in2touch-react/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/in2touch-react/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/in2touch-react/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/in2touch-react/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/in2touch-react/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/in2touch-react/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/in2touch-react/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/in2touch-react/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/in2touch-react/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/in2touch-react/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/in2touch-react/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/in2touch-react/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  }
+]);
--- src/components/layouts/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layout = ({ ...props }) => (
-  <div className="Pagewrap">
-    {props.children}
-  </div>
-)
-
-export default Layout
